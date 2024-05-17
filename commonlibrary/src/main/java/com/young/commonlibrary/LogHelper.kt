8018866b34b2f292d509e19e50db712d9586848c
smilekoko <179621228@qq.com>
2024-05-17 14:37:25 +0800

    创建公共库

---
+++ commonlibrary/src/main/java/com/young/commonlibrary/LogHelper.kt
@@ -0,0 +1,11 @@
+package com.young.commonlibrary
+
+import android.util.Log
+
+object LogHelper {
+
+    fun logI(msg:String,tag:String=""){
+        Log.i(tag,msg)
+    }
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "AndroidDemo"
 include(":app")
- 
+include(":commonlibrary")
